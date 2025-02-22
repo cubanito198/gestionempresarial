9859767015ce8c325f48bdf5cd724314692dd69f
luis sanchez diaz <129956675+cubanito198@users.noreply.github.com>
2025-02-22 14:23:24 +0100

    Add files via upload

---
+++ supercontrolador/api/cliente/index.php
@@ -0,0 +1,22 @@
+<?php include "inc/morir.php" ?>
+<?php include "inc/idiomas.php" ?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>
+      ERP luis sanchez diaz
+    </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilologin.css">
+    <script src="login.js"></script>
+  </head>
+  <body>
+    <div id="formlogin">
+      <img src="img/logo.svg" id="logo">
+      <input type="text" id="usuario" placeholder="Usuario:">
+      <input type="text" id="contrasena" placeholder="Contraseña">
+      <button id="login">Login</button>
+      <div id="feedback"></div>
+    </div>
+  </body>
+</html>
